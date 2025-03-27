001cf65d51b77f88a76c8a0c7e48bfd29ac408a3
alex boyle <aboyle75@gmail.com>
2025-03-27 19:13:54 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Components/Header";
 import Home from "./Pages/Home";
-import Payment from "./Pages/Payment";
-import Events from "./Pages/Events";
-import Social from "./Pages/Social";
 import ExamplePage from "./Pages/ExamplePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,14 +8,9 @@ function App() {
 	return (
 		<div className="App">
 			<Router>
-				<Header />
 				<div style={{ maxWidth: "1000px", margin: "auto", overflow: "hidden" }}>
 					<Routes>
 						<Route path="/" exact element={<Home />} />
-						<Route path="/payment" element={<Payment />} />
-						<Route path="/events" element={<Events />} />
-						<Route path="/social" element={<Social />} />
-						<Route path="/*" element={<ExamplePage />} />
 					</Routes>
 				</div>
 			</Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,18 +3,18 @@ import "./Home.css";
 function ExamplePage() {
 	return (
 		<>
-			<div className="bg" style={{ height: "500px" }}></div>
-			<h1>CLIMBING FOR ALL</h1>
-			<div>
-				This group is a great group of climbers with abilities ranging from
-				beginner to expert. We will meet weekly on Wednesdays at Movement
-				Climbing in Lincoln Park unless stated otherwise. Anyone of any climbing
-				ability is welcome! You can boulder or belay! We are not affiliated with
-				Movement or any other gym and their standard dues/membership rates
-				apply. This group has no dues.
-			</div>
-			<br />
-			<a href="https://www.meetup.com/606-climbing/">JOIN THE MEETUP!</a>
+			<div class="bg-white rounded-lg shadow-lg">
+                  <div class="p-4 text-center">
+                      <img class="rounded-full mx-auto border" src="images/606.png" alt="Profile Picture" style="width:100px;height:100px;" />
+                      <h2 class="text-2xl font-semibold mt-2">606 Climbing</h2>
+                      <div class="mt-4">
+                          <a href="https://meetup.com/606-climbing" class="block py-2 px-4 rounded text-white font-bold mt-3 transition-colors duration-300" style="background-color: #e2373f;" onmouseover="this.style.backgroundColor='#cc2f32'" onmouseout="this.style.backgroundColor='#e2373f'">606 Climbing | Meetup</a>
+                          <a href="https://www.instagram.com/606climbing" class="block py-2 px-4 rounded text-white font-bold mt-3 transition duration-300 ease-in-out transform hover:scale-105" style="background-color: #C13584;">Instagram</a>
+                          <a href="https://chat.whatsapp.com/GKfHxnUB9ZT9IGnPbsFgYA" class="block py-2 px-4 rounded text-white font-bold mt-3 transition duration-300 ease-in-out transform hover:scale-105" style="background-color: #25D366;">Climbing WhatsApp</a>
+                          <a href="https://chat.whatsapp.com/HrJgSyLyLfM94dRMQNxxoY" class="block py-2 px-4 rounded text-white font-bold mt-3 transition duration-300 ease-in-out transform hover:scale-105" style="background-color: #25D366;">Cycling Group WhatsApp</a>
+                      </div>
+                  </div>
+              </div>
 		</>
 	);
 }
